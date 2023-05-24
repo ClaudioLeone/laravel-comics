0472bffa6ac3b6751e917a1a9ea8745c38fafa38
ClaudioLeone <claleone92@gmail.com>
2023-05-24 16:19:37 +0200

    Installed npm dipendences, Bootstrap, created 'app' layout file, created 'header' and 'footer' partials, extended 'welcome' page and linked page sections to 'app.blade.php'

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,20 @@
+<footer>
+    <div>
+        <button type="submit">SIGN-UP NOW</button>
+    </div>
+
+    <div>
+        <div>
+            <h6>FOLLOW US</h6>
+        </div>
+        <div>
+            <ul>
+                <li><a href="">F</a></li>
+                <li><a href="">T</a></li>
+                <li><a href="">Y</a></li>
+                <li><a href="">P</a></li>
+                <li><a href="">M</a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
